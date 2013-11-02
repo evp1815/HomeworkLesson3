c588cfb7f00d8a0d512d979e4f3c449cb162c659
eliotik <eliotik@gmail.com>
2013-11-02 18:15:37 +0200

    class for Plafond

---
+++ LampPlafond.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Class LampPlafond
+ */
+class LampPlafond {
+    private $color;
+
+    /**
+     * @return mixed
+     */
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    /**
+     * @param mixed $color
+     */
+    public function setColor($color)
+    {
+        $this->color = $color;
+    }
+
+}
